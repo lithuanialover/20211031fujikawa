cac844c4873c387736d2f8b45909b073217e6939
lithuanialover <fujikawaayana@gmail.com>
2021-11-06 19:55:42 +0900

    first-commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-var a = [2, 4, 7, 5, 4, 3, 8];
-
-//重複を削除
-var b = a.filter(function (x, i, self) {
-  return self.indexOf(x) === i;
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
 });
-
-console.log(b);
